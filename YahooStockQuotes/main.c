f390db5686d491a6991e163589a5e794cd82de93
LaVelton R. Carroway <lrcarroway@gmail.com>
2014-07-15 16:42:41 -0500

    Initial Commit

---
+++ YahooStockQuotes/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  YahooStockQuotes
+//
+//  Created by LaVelton R. Carroway on 07/15/2014.
+//  Copyright (c) 2014 Rochee Software, LLC. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
